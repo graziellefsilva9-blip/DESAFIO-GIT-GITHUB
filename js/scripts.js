f3116759bed2749da36de432a5d4088dd588ddd6
Grazielle <grazielle.f.silva9@aluno.senai.br>
2026-06-11 16:41:17 -0300

    acrecentado o console no js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,76 @@
 console.log("teste")
+
+console.log("Comando de saída")
+
+//Comentario de linhas simples
+
+/* cometario de varias linhas
+
+
+*/
+
+//Criando variáveis
+let num = 50
+var num2 = 180
+const num3 = 450
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+if (true){
+    let num4 =420
+}
+console.log(num4)
+
+//num3 = 12
+
+//CONCATENAÇÃO
+console.log("Valor de num" + num)
+console.log('Valor de num3', num3)
+console.log(`Valor de num2 ${num2}`)
+console.log('A soma dos números ' + 15 + 8) 
+console.log('A soma dos números', 15+8)
+console.log(`A soma dos números ${15 + 8}`)
+console.log('A soma dos números' + num + num2)
+console.log('A soma dos números', num + 8)
+console.log(`A soma dos números $ num3`)
+
+//OPERADORES MATEMATICOS
+/** 
+ * +SOMA
+ * -SUBTRAÇÃO
+ * *MULTIPLICAÇÃO
+ * /DIVISAÕ
+ * %MÓDULO (RESTO DA DIVISÃO)
+ */
+console.log('SOMA DE VARIAVEIS', num + num2)
+console.log('SUBTRAÇÃO DAS VARIAVEIS', num3 -num)
+console.log('Multiplicação', num5 * 2)
+console.log('DIVISÃO DE VALORES', num2 / 5)
+console.log('RESTO DA DIVISÃO', num2 % 2)
+console.log('RESTO DA DIVISÃO ${num3 % 3}')
+
+//OPERADORES RELACIONAIS
+/*
+     == IGUAL
+     > MAIOR
+     < MENOR
+     >= MAIOR IGUAL
+     <= MENOR IGUAL
+     != DIFERENTE
+*/
+
+//OPERADORES LÓGICO
+/* 
+&& E
+|| OU
+*/
+
+console.log('TESTE DE RELACIONAIS', 10 == 5)
+console.log('TESTE DE RELACIONAIS', 10 != 5)
+console.log('TESTE DE RELACIONAIS', 10> 5)
+console.log('TESTE DE RELACIONAIS', 10< 5)
+console.log('TESTE DE RELACIONAIS', 10 >= 5)
+console.log('TESTE DE RELACIONAIS',10 <= 5)
+console.log('TESTE DE RELACIONAIS')
+console.log('TESTE DE RELACIONAIS')
