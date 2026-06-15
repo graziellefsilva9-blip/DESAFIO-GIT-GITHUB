6bc434a8e493f8ed02ef9ea931a88cd4d78a97b0
Grazielle <grazielle.f.silva9@aluno.senai.br>
2026-06-15 13:32:45 -0300

    caculo de media

---
+++ js/script_questao01.js
@@ -0,0 +1,25 @@
+//PEGANDO LEMENTOS DO DOM
+const formDados = document.querySelector('#formulario-pessoa')
+const divResultado =  document.querySelector('#div-dados')
+
+//CAPTURANDO O EVETO SUBMIT DO FORMULARIO
+ formDados.addEventListener ('submit', (evt)=>{
+    evt.preventDefault()
+    
+    const objformDados = new FormData(formDados)
+
+    let nome = objformDados.get('nome')
+    let idade = objformDados.get('idade')
+
+    let situacaoIdade = ''
+
+    if (idade >= 18){
+        situacaoIdade = `${nome}, você é maior de idade`
+    }else{
+        situacaoIdade = `${nome}, você é menor de idade`
+    }
+
+    divResultado.innerHTML = situacaoIdade
+
+
+})
